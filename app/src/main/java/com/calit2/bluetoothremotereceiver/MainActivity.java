2aa777f67be7d4c1dac8d6e20c7695a2a868d5fe
Jacob Terrado <jsterrad@ucsd.edu>
2016-01-18 14:32:00 -0800

    Initial commit

---
+++ app/src/main/java/com/calit2/bluetoothremotereceiver/MainActivity.java
@@ -0,0 +1,41 @@
+package com.calit2.bluetoothremotereceiver;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+
+/******************************************************************************
+ * Name:        MainActivity
+ * Description: Activity that determines which Fragment to display as the
+ *              screen of the application.
+ *****************************************************************************/
+public class MainActivity extends FragmentActivity {
+
+  // Log Message
+  private static final String TAG = "MainActivity";
+
+  /****************************************************************************
+   * Name:        onCreate
+   * Description: Determines appropriate Fragment to display based on user's
+   *              input
+   *
+   * @param       savedInstanceState Reference to Bundle object. Activities
+   *                                 can be restored to a former state using
+   *                                 data saved in this bundle.
+   ***************************************************************************/
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      FragmentTransaction transaction = getSupportFragmentManager()
+          .beginTransaction();
+      // BluetoothReceiverFragment fragment = BluetoothReceiverFragment.newInstance();
+      BluetoothRemoteFragment fragment = BluetoothRemoteFragment.newInstance();
+      transaction.replace(R.id.content_fragment, fragment);
+      transaction.commit();
+    }
+  }
+}
